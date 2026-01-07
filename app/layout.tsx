fc1e77abddfa7198afeecbd68474e11387cbdbe1
Cleber Silva <noct@MacBook-Pro.local>
2026-01-07 14:39:07 -0300

    feat add site ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+const faviconDataUrl =
+  "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzRFOjEBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc1Nzc3NzctNzU3Nzc3Nzc3NzcrNzctNzc3Nzc3NzctKyw0LSstKzcuN//AABEIABwAHAMBIgACEQEDEQH/xAAZAAEAAgMAAAAAAAAAAAAAAAAEAwUBAgf/xAApEAACAQMCBQMFAQAAAAAAAAABAgMABBESMQUTIVFhIkFxMkKRocEU/8QAFwEBAQEBAAAAAAAAAAAAAAAAAQIDAP/EABgRAQADAQAAAAAAAAAAAAAAAAEAAhEh/9oADAMBAAIRAxEAPwDsl9cSLNFbW2OdLk6iMhFG5qUQyrHhZ3L93AI/AoUUok4vHIw067YjSd1YMMj90i6v0iBjgHPuPtiTqc+ew8mqSZidWb8Puv8AXBrK6HVijrnZgcGlVT8HMkNrIqxmZ1kYyuCBqfPqC98be21WdtcR3MKyxHKt3GCO4PmhOxpbQ2QcQtTOivGFM0R1JqHQ9wfBrNlcQSqUjURyL9cRGGU/H9pBO1QXNtBcgGeFHK7EjqPg10WvdIVmXhCTPIQ1szl1APqVjuAPfrt71DwmS4tbU86yuGklkeVgoXCajnHUimwWFrE/NSEGRdnYliPgnamU7JKO7P/Z";
+
 export const metadata: Metadata = {
   title: "Cervi Bazar | Dashboard",
   description: "Interface do sistema de caixa e vendas.",
+  icons: {
+    icon: faviconDataUrl,
+  },
 };
 
 export default function RootLayout({
